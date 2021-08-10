3eced11de16b287146a330f54f2c3cdabc175dbb
Sia2602 <danilojose26@hotmail.com>
2021-08-10 18:07:24 -0300

    Adicionando main base.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,10 @@
+/*  Referencias:
+    https://en.cppreference.com/w/cpp/thread/thread/native_handle
+    https://pubs.opengroup.org/onlinepubs/7908799/xsh/signal.h.html
+    https://github.com/csimmonds/periodic-threads/blob/master/timer.c
+    https://2net.co.uk/tutorial/periodic_threads
+*/
+
 #include <thread>
 #include <mutex>
 #include <iostream>
@@ -20,23 +27,26 @@ void f(int num)
 }
  
 int main()
-{
-    std::thread t1(f, 1);  
- 
-    sched_param sch;    
+{          
+    unsigned int periodo = 500; //Periodo da thread periodica
+	unsigned int cargaCPU = 100000; //Carga da CPU
 
-    //Para comportamento periodico no Linux
+    // Sinal que sera enviado do timer Posix para implementar comportamento periodico da thread
+    // SIGALRM: Alarm clock.
     sigset_t alarm_sig;
 	sigemptyset(&alarm_sig); //Initialize a set to contain no signals
 	sigaddset(&alarm_sig, SIGALRM); //Add a signal to a set
 	sigprocmask(SIG_BLOCK, &alarm_sig, NULL);//Add the signals pointed to by set to the thread mask.
     
-    int policy; 
-    pthread_getschedparam(t1.native_handle(), &policy, &sch);
-    sch.sched_priority = 20; //de 1 a 99
+    sched_param sch; //objeto que ira definir o grau de prioridade para thread 
+    int policy; // location where the function can store the scheduling policy
+    std::thread t1(f, 1);  //criando thread 
+    pthread_getschedparam(t1.native_handle(), &policy, &sch); //Ref.: http://www.qnx.com/developers/docs/6.5.0SP1.update/com.qnx.doc.neutrino_lib_ref/p/pthread_getschedparam.html
+    sch.sched_priority = 60; //prioridade de 1 a 99
     
+    //Estabelencendo politica de escalonamento para a thread para SCHED_FIFO
     if (pthread_setschedparam(t1.native_handle(), SCHED_FIFO, &sch)) {
-        std::cout << "Failed to setschedparam: " << std::strerror(errno) << '\n';
+        std::cout << "Falha ao setar politica de escalonamento: " << std::strerror(errno) << '\n';
     }
  
     t1.join();
